b96da2bc331aac22ade99ecc26a50df69599b089
Eric Pedley <ericpedley@gmail.com>
2023-06-01 17:27:10 -0700

    finish time benchmarking

--- perf_benchmark.py
+++ perf_benchmark.py
@@ -4,20 +4,23 @@ import torch
 
 from model import SUASYOLO
 
-model = SUASYOLO(num_classes = 17).to("cuda")
+model = SUASYOLO(num_classes = 14).to("cuda")
 
 # @profile
-def test(num_repeats=100, batch_size=1):
+def test(num_repeats=200, batch_size=1):
     start =  time.perf_counter()
     for _ in range(num_repeats):
         x = torch.rand((batch_size, 3, 640, 640))
         x=x.to("cuda")
         boxes, classes, objectness = model.predict(x)
     end = time.perf_counter()
-    print(f"Time taken: {end-start}")
+    print(f"Batch size {batch_size}: Time taken: {end-start} ({(end-start)/batch_size} per {num_repeats} images)")
 
-print("Single batch")
-test(num_repeats=100, batch_size=4)
+test(batch_size=1)
+test(batch_size=2)
+test(batch_size=4)
+test(batch_size=6)
+test(batch_size=7)
 # print("Batch size 4")
 # test(num_repeats=100, batch_size=4)
 # print("Batch size 8")
