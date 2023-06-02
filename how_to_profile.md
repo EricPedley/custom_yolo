0935a3fc47a50b890bf0fccdb840edb86dc1c6db
Eric Pedley <ericpedley@gmail.com>
2023-06-01 17:40:11 -0700

    add memory profiling

---
+++ how_to_profile.md
@@ -0,0 +1,4 @@
+# Memory Profiling
+1. `pip install memory_profiler`
+2. Decorated functions with @profile
+3. `py -m memory_profiler my_script.py`
--- perf_benchmark.py
+++ perf_benchmark.py
@@ -1,27 +1,30 @@
 import time
 
 import torch
+from tqdm import tqdm
 
 from model import SUASYOLO
 
-model = SUASYOLO(num_classes = 14).to("cuda")
+USE_CUDA = False
+device = "cuda" if USE_CUDA and torch.cuda.is_available() else "cpu"
+
+model = SUASYOLO(num_classes = 14).to(device)
 
 # @profile
-def test(num_repeats=200, batch_size=1):
+def test(num_repeats=10, batch_size=1):
+    print(f"Testing batch size {batch_size}")
     start =  time.perf_counter()
-    for _ in range(num_repeats):
+    for _ in tqdm(range(num_repeats)):
         x = torch.rand((batch_size, 3, 640, 640))
-        x=x.to("cuda")
+        x=x.to(device)
         boxes, classes, objectness = model.predict(x)
     end = time.perf_counter()
-    print(f"Batch size {batch_size}: Time taken: {end-start} ({(end-start)/batch_size} per {num_repeats} images)")
+    print(f"Time taken: {end-start} ({(end-start)/batch_size} per {num_repeats} images)")
 
-test(batch_size=1)
-test(batch_size=2)
-test(batch_size=4)
-test(batch_size=6)
-test(batch_size=7)
-# print("Batch size 4")
-# test(num_repeats=100, batch_size=4)
-# print("Batch size 8")
-# test(num_repeats=100, batch_size=8)
+@profile
+def run_all_tests():
+    test(batch_size=1)
+    test(batch_size=2)
+    test(batch_size=4)
+
+run_all_tests()
