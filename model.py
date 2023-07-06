65acdadcb3bbabb9c23d32d84ec7a593c36f5695
Eric Pedley <ericpedley@gmail.com>
2023-07-05 18:26:21 -0700

    naive implementation of yolov8 backbone

--- model.py
+++ model.py
@@ -21,6 +21,80 @@ def flatten(l):
       out.append(item)
   return out
 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+class SPPF(nn.Module):
+    """Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher."""
+
+    def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+
+    def forward(self, x):
+        """Forward pass through Ghost Convolution block."""
+        x = self.cv1(x)
+        y1 = self.m(x)
+        y2 = self.m(y1)
+        return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
+
+class Bottleneck(nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):  # ch_in, ch_out, shortcut, groups, kernels, expand
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """'forward()' applies the YOLOv5 FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+
+class C2f(nn.Module):
+    """CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+
 class DWConv(nn.Module):
     '''
     Drop-in replacement for nn.Conv2d
@@ -100,18 +174,16 @@ class SUASYOLO(nn.Module):
         super(SUASYOLO, self).__init__()
         self.num_classes = num_classes
         self.feature_extraction = nn.Sequential(
-            ConvLayer(3, 32, kernel_size=3, stride=1, padding=1), 
-            ConvLayer(32, 64, kernel_size=3, stride=2, padding=1),
-            ResBlock(64, num_repeats=1),
+            ConvLayer(3, 64, kernel_size=3, stride=1, padding=1), 
             ConvLayer(64, 128, kernel_size=3, stride=2, padding=1),
-            ResBlock(128, num_repeats=2),
+            *[C2f(128, 128) for _ in range(3)],
             ConvLayer(128, 256, kernel_size=3, stride=2, padding=1),
-            ResBlock(256, num_repeats=8),
+            *[C2f(256, 256) for _ in range(6)],
             ConvLayer(256, 512, kernel_size=3, stride=2, padding=1),
-            ResBlock(512, num_repeats=8),
+            *[C2f(512, 512) for _ in range(6)],
             ConvLayer(512, 1024, kernel_size=3, stride=2, padding=1),
-            ResBlock(1024, num_repeats=4),
-            ConvLayer(1024, 1024, kernel_size=3, stride=2, padding=1)
+            *[C2f(1024, 1024) for _ in range(3)],
+            SPPF(1024, 1024, 5)
         )
         # automatically calculate number of stride 2 convs in feature extraction
         num_size_reductions = 0
