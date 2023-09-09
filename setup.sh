964a93fdb736c375b49754c2234c769c8144a12c
Eric Pedley <ericpedley@gmail.com>
2023-09-08 22:55:32 -0700

    make unzip download separately from everything else

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt & PIDPIP=$!
-apt-get update && apt-get install tmux vim unzip ffmpeg libsm6 libxext6  -y & PIDAPT=$!
+apt-get update && apt-get install unzip & PIDAPT=$!
+apt-get install tmux vim ffmpeg libsm6 libxext6  -y &
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qVfVh2gGTG742C6J0l9WB-aBuJkcb1va' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qVfVh2gGTG742C6J0l9WB-aBuJkcb1va" -O data_v2.zip && rm -rf /tmp/cookies.txt & PIDGET=$!
 wait $PIDGET
 wait $PIDAPT
@@ -7,3 +8,4 @@ unzip data_v2.zip
 mkdir runs
 wait $PIDPIP
 alias py=python3
+wait # for miscellaneous apt packages to install
